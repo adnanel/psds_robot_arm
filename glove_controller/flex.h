2b6a31b388c3118289cc17a55c73aee3a6ad628e
adnan <adnanel94@gmail.com>
2018-05-28 22:28:41 +0200

    calibration

--- glove_controller/flex.h
+++ glove_controller/flex.h
@@ -2,13 +2,26 @@
 #define PSDS_FLEX_H
 
 class Flex {
+public:
+	struct CalibrationData {
+		float vcc;
+		float rDiv;
+		float straightResistance;
+		float BendResistance;
+	};
+private:
 	int targetPin;
 
-	static constexpr float VCC = 4.98;
-	static constexpr float R_DIV = 47500.0;
-	static constexpr float STRAIGHT_RESISTANCE = 37300.0;
-	static constexpr float BEND_RESISTANCE = 90000.0;
+	static constexpr float DEFAULT_VCC = 4.98;
+	static constexpr float DEFAULT_R_DIV = 47500.0;
+	static constexpr float DEFAULT_STRAIGHT_RESISTANCE = 37300.0;
+	static constexpr float DEFAULT_BEND_RESISTANCE = 90000.0;
 
+
+	float vcc = 4.98;
+	float rDiv = 47500.0;
+	float straightResistance = 37300.0;
+	float BendResistance = 90000.0;
 public:
 	inline Flex(int pin) {
 		this->targetPin = pin;
@@ -16,6 +29,12 @@ public:
 		pinMode(pin, INPUT);
 	}
 
+	inline void calibrate(const CalibrationData& calibData) {
+		this->vcc = calibData.vcc;
+		this->rDiv = rcalibData.rDiv;
+		this->straightResistance = straightResistacalibData.straightResistance;
+		this->BendResistance = BendResistacalibData.BendResistance;
+	}
 
 	inline float getAngle() {
 		int flexADC = analogRead(targetPin);
--- glove_controller/glove_controller.ino
+++ glove_controller/glove_controller.ino
@@ -1,18 +1,19 @@
 #include <Servo.h>
 #include <SPI.h>
 #include <SoftwareSerial.h>
+#include <EEPROM.h>
 
 #include "flex.h"
 
 SoftwareSerial BTSerial(10, 11); // RX | TX
 
-const int flexCount = 1;
+const int flexCount = 5;
 const Flex flexes[]= {
         Flex(A2),
         Flex(A3),
         Flex(A4),
-        Flex(A5)
-        //Flex(A6)
+        Flex(A5),
+        Flex(A6)
 };
 
 void setup() {
@@ -21,6 +22,34 @@ void setup() {
 
   
   BTSerial.begin(9600);
+
+
+  // Load calibration
+  char magic;
+  EEPROM.get(0, magic);
+  if ( magic == 101 ) {
+    int addr = 1;
+    for (int i = 0; i < flexCount; ++i) {
+      float vcc;
+      float rDiv;
+      float straightResistance;
+      float BendResistance;
+
+      EEPROM.get(addr, vcc);
+      EEPROM.get(addr + sizeof(float) * 1, rDiv);
+      EEPROM.get(addr + sizeof(float) * 2, straightResistance);
+      EEPROM.get(addr + sizeof(float) * 3, BendResistance);
+
+      flexes[i].calibrate({
+                                  vcc,
+                                  rDiv,
+                                  straightResistance,
+                                  BendResistance
+                          });
+
+      addr += sizeof(float) * 4;
+    }
+  }
 }
 
 void loop() {
@@ -41,6 +70,10 @@ void loop() {
 
   if (BTSerial.available()) {
     Serial.write(BTSerial.read());
+  }
+
+  if ( Serial.available() ) {
+
   }
 }
 
